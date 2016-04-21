32537a2a3edf46e8d31d5b2e04019c9c24239343
sai-y <sai@altschool.com>
2016-04-20 19:35:55 -0700

    Chapter_2_check_in

---
+++ chapter_2/input_test.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+address = input("What is your address? ")
+age = input("How old are you? ")
+
+
+print("My name is " + name)
+print("I am " + age + " years old")
+print("My address is " + address)
---
+++ chapter_2/while_loop.py
@@ -0,0 +1,7 @@
+i=0
+j=0
+while i<10:
+  while j<10:
+    print("The value of i,j is ",i,",",j)
+    i+=1
+    j+=1
