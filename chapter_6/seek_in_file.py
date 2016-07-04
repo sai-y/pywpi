0a0ef390f3fc9dcb680b6e845b1dc74ae9a9aa0b
Sai Yamanoor <sai@Sais-MBP-14806.lan>
2016-07-04 12:32:10 -0700

    Checking chapter 6 code

---
+++ chapter_6/seek_in_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+	# open text file to read 
+
+	file = open('write_file.txt', 'r')
+	
+	# read a line from the file 
+	file.write('This is a line appended to the file\n')
+	file.seek(5)
+	
+	data = file.read()
+	print(data)
+	file.close()
