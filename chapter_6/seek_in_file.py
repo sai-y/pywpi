78e1d467edac833a66668c569d0f19a43d87d566
Sai Yamanoor <sai@Sais-MBP-14806.lan>
2016-07-04 18:27:22 -0700

    Checking in seek example

--- chapter_6/seek_in_file.py
+++ chapter_6/seek_in_file.py
@@ -6,8 +6,7 @@ if __name__ == "__main__":
 
 	file = open('write_file.txt', 'r')
 	
-	# read a line from the file 
-	file.write('This is a line appended to the file\n')
+	# read the second line from the file 
 	file.seek(54)
 	
 	data = file.read()
