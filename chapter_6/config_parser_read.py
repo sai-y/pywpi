114242550a3db42e0b534137bdb212f44bc32865
sai-y <yamanoorsai@gmail.com>
2016-07-05 20:57:51 -0700

    Config Parser Read correction 2

--- chapter_6/config_parser_read.py
+++ chapter_6/config_parser_read.py
@@ -6,15 +6,15 @@ if __name__ == "__main__":
 	# initialize ConfigParser
 	config_parser = configparser.ConfigParser()
 
-	# Let's create a config file
-	with open('raspi.cfg', 'r') as config_file:
-		#let's read constants from the App Info section
-		device_id = config_parser.get('AppInfo', 'id')
-		debug_switch = config_parser.get('AppInfo', 'debug_switch')
-		sensor_address = config_parser.get('AppInfo', 'sensor_address')
+	# Let's read the config file
+	config_parser.read('raspi.cfg')
 
-		# execute the code if the debug switch is true
-		if debug_switch == "True": 
-			print("The device id is " + device_id)
-			print("The sensor_address is " + sensor_address)
+	device_id = config_parser.get('AppInfo', 'id')
+	debug_switch = config_parser.get('AppInfo', 'debug_switch')
+	sensor_address = config_parser.get('AppInfo', 'sensor_address')
+
+	# execute the code if the debug switch is true
+	if debug_switch == "True": 
+		print("The device id is " + device_id)
+		print("The sensor_address is " + sensor_address)
 
