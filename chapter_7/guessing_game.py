f27765e2d05d5db98d46b23a5836734d3edd1ec3
sai-y <yamanoorsai@gmail.com>
2016-07-08 00:27:45 -0700

    Guessing game cleanup

--- chapter_7/guessing_game.py
+++ chapter_7/guessing_game.py
@@ -5,7 +5,11 @@ import random
 if __name__ == "__main__":
     while True:
         rand_num = random.randrange(0,10)
-        input_value = int(input("Enter a number between 0 and 10: "))
+        value = input("Enter a number between 0 and 10: ")
+        if value == 'x':
+            print("Thanks for playing! Bye!")
+            break
+        input_value = int(value)
         if input_value == rand_num:
             print("Your guess is correct! You win!")
             break
