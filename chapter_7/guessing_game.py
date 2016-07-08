923082d871d54594e74636ee50bb05645ea06c76
sai-y <yamanoorsai@gmail.com>
2016-07-08 00:21:58 -0700

    Guessing game correction

--- chapter_7/guessing_game.py
+++ chapter_7/guessing_game.py
@@ -5,7 +5,7 @@ import random
 if __name__ == "__main__":
     count = 0
     running_average = 0
-	while True:
+    while True:
         rand_num = random.randrange(0,10)
         input_value = int(input("Enter a number between 0 and 10: "))
         if input_value == input_value:
