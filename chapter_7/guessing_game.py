d635361894ad3e6223c244f05d30fee3bb53f28e
sai-y <yamanoorsai@gmail.com>
2016-07-08 00:19:40 -0700

    Guessing game

---
+++ chapter_7/guessing_game.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import random
+
+if __name__ == "__main__":
+    count = 0
+    running_average = 0
+	while True:
+        rand_num = random.randrange(0,10)
+        input_value = int(input("Enter a number between 0 and 10: "))
+        if input_value == input_value:
+            print("Your guess is correct! You win!")
+            break
+
+        
+        
+
