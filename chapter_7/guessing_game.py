8fb570524b8649c3dea6be8a2c3e5bfaf307313d
sai-y <yamanoorsai@gmail.com>
2016-07-08 00:23:36 -0700

    Guessing game enhancement

--- chapter_7/guessing_game.py
+++ chapter_7/guessing_game.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     while True:
         rand_num = random.randrange(0,10)
         input_value = int(input("Enter a number between 0 and 10: "))
-        if input_value == input_value:
+        if input_value == rand_num:
             print("Your guess is correct! You win!")
             break
+        else:
+            print("Nope! The random value was %s" % rand_num)
 
         
         
