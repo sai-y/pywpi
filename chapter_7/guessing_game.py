45a106b8d8bad0e3619f555ed3a885af15121831
sai-y <yamanoorsai@gmail.com>
2016-07-08 00:25:09 -0700

    Guessing game cleanup

--- chapter_7/guessing_game.py
+++ chapter_7/guessing_game.py
@@ -3,8 +3,6 @@
 import random
 
 if __name__ == "__main__":
-    count = 0
-    running_average = 0
     while True:
         rand_num = random.randrange(0,10)
         input_value = int(input("Enter a number between 0 and 10: "))
