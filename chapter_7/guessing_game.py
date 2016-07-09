82107dc734f90e21e98237e437e5e84f8e3c19fa
sai-y <yamanoorsai@gmail.com>
2016-07-09 13:59:45 -0700

    Guessing game correction

--- chapter_7/guessing_game.py
+++ chapter_7/guessing_game.py
@@ -4,12 +4,18 @@ import random
 
 if __name__ == "__main__":
     while True:
+        # generate a random number between 0 and 9
         rand_num = random.randrange(0,10)
-        value = input("Enter a number between 0 and 10: ")
+
+        # prompt the user for a number
+        value = input("Enter a number between 0 and 9: ")
+        
         if value == 'x':
             print("Thanks for playing! Bye!")
             break
+        
         input_value = int(value)
+        
         if input_value == rand_num:
             print("Your guess is correct! You win!")
             break
