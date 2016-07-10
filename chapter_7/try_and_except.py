95c7176df5da4a7665e9994e7b6665d63c4dee3d
sai-y <yamanoorsai@gmail.com>
2016-07-09 18:42:45 -0700

    Try and Except Example

--- chapter_7/try_and_except.py
+++ chapter_7/try_and_except.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 
+import random
+
 if __name__ == "__main__":
-	input_num = input("Enter a number between 0 and 10: ")
-	try:
-		val = int(input_num)
-		if val > 0 and val < 10: 
-			print("The value is valid")
-	except ValueError as error: 
-		print("The value is invalid %s" % error)
+    while True:
+        # generate a random number between 0 and 9
+        rand_num = random.randrange(0,10)
+
+        # prompt the user for a number
+        value = input("Enter a number between 0 and 10: ")
+
+        if value == 'x':
+            print("Thanks for playing! Bye!")
+
+        try:
+            input_num = int(value)
+
+            if input_value == rand_num:
+                print("Your guess is correct! You win!")
+                break
+            else:
+                print("Nope! The random value was %s" % rand_num)
+            
+        except ValueError as error: 
+            print("The value is invalid %s" % error)
