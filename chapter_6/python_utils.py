67e97b2c19d21f4a9e47849d6cbce7c51d28ebf3
sai-y <yamanoorsai@gmail.com>
2016-07-10 18:57:26 -0700

    Correction to python utils

--- chapter_6/python_utils.py
+++ chapter_6/python_utils.py
@@ -3,7 +3,7 @@
 import os
 
 if __name__ == "__main__":
-    if os.path.isfile('/home/pi/Desktop/write_file.txt'):
+    if os.path.isfile('/home/pi/Desktop/code_samples/write_file.txt'):
         print('The file exists!')
     else:
         print('The file does not exist!')
