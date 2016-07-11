a651bfaa07a0484a6eb00b6c5bf11b1d32640bc2
sai-y <yamanoorsai@gmail.com>
2016-07-10 20:52:23 -0700

    os isdir check in

--- chapter_6/python_utils.py
+++ chapter_6/python_utils.py
@@ -3,7 +3,14 @@
 import os
 
 if __name__ == "__main__":
+    # Check if file exists
     if os.path.isfile('/home/pi/Desktop/code_samples/write_file.txt'):
         print('The file exists!')
     else:
         print('The file does not exist!')
+
+    # Confirm code_samples' existence
+    if os.path.isdir('/home/pi/Desktop/code_samples'):
+        print('The directory exists!')
+    else:
+        print('The directory does not exist!')
