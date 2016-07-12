fb60ffcea63596ae0efe753459f43d2f3d8fec9f
sai-y <yamanoorsai@gmail.com>
2016-07-11 20:01:23 -0700

    Glob example

--- chapter_6/glob_example.py
+++ chapter_6/glob_example.py
@@ -4,5 +4,5 @@ import glob
 
 if __name__ == "__main__":
 	# List all files
-    for file in glob.glob('/home/pi/Desktop/code_samples/chapter_6'):
-    	print file
+    for file in glob.glob('*.py'):
+    	print(file)
