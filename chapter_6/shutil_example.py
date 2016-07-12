328bbca0525781747f0bfdad092f3cc70220d12d
sai-y <yamanoorsai@gmail.com>
2016-07-12 00:05:33 -0700

    shutil addition

--- chapter_6/shutil_example.py
+++ chapter_6/shutil_example.py
@@ -2,6 +2,7 @@
 
 import glob
 import shutil
+import os
 
 if __name__ == "__main__":
 
@@ -9,4 +10,5 @@ if __name__ == "__main__":
     	shutil.move(file, '.')
 
     for file in glob.glob('file1[0-9][0-9].txt'):
-    	shutil.copy(file, 'cpy_files')
+    	shutil.copy(file, 'txt_files')
+    	os.remove(file)
