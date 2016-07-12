a9a67b02ac33613d055b74e22eafba5dbb49caec
sai-y <yamanoorsai@gmail.com>
2016-07-12 00:07:42 -0700

    shutil comments

--- chapter_6/shutil_example.py
+++ chapter_6/shutil_example.py
@@ -6,9 +6,11 @@ import os
 
 if __name__ == "__main__":
 
+	# move files to the current directory
     for file in glob.glob('txt_files/file1[0-9][0-9].txt'):
     	shutil.move(file, '.')
 
+    # make a copy of files in txt_files and delete them
     for file in glob.glob('file1[0-9][0-9].txt'):
     	shutil.copy(file, 'txt_files')
     	os.remove(file)
