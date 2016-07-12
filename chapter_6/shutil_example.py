9ad5ed178099d0482dfe0a5d320836ec35e445ee
sai-y <yamanoorsai@gmail.com>
2016-07-11 23:54:44 -0700

    shutil example

---
+++ chapter_6/shutil_example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import glob
+import shutil
+
+if __name__ == "__main__":
+
+    for file in glob.glob('txt_files/file1[0-9][0-9].txt'):
+    	shutil.move(file, '.')
+
+    for file in glob.glob('file1[0-9][0-9].txt'):
+    	shutil.copy(file, 'txt_files')
