984f940b66ac2470327e58cac2f3324a7e1853e4
sai-y <yamanoorsai@gmail.com>
2016-07-27 23:58:59 -0700

    Correction to try and except

--- chapter_7/try_and_except.py
+++ chapter_7/try_and_except.py
@@ -15,15 +15,16 @@ if __name__ == "__main__":
 
         try:
             input_value = int(value)
-
-            if input_value < 0 or input_value > 10:
-                print("Input invalid. Enter a number between 0 and 9.")
-
-            if input_value == rand_num:
-                print("Your guess is correct! You win!")
-                break
-            else:
-                print("Nope! The random value was %s" % rand_num)
-            
         except ValueError as error: 
             print("The value is invalid %s" % error)
+            continue
+
+        if input_value < 0 or input_value > 10:
+            print("Input invalid. Enter a number between 0 and 9.")
+            continue
+
+        if input_value == rand_num:
+            print("Your guess is correct! You win!")
+            break
+        else:
+            print("Nope! The random value was %s" % rand_num)
