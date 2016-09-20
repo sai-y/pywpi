f87e745d31450d5f6869dc2bf516702156e61134
sai-y <yamanoorsai@gmail.com>
2016-09-20 16:37:23 -0700

    Internet access example

--- chapter_7/internet_access.py
+++ chapter_7/internet_access.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-import urllib 
+from urllib import request
+from urllib.error import URLError
 
 if __name__ == "__main__":
   # Source for link: http://stackoverflow.com/a/30635751/822170
   try:
     response = urllib.request.urlopen('http://nist.time.gv/actualtime.cgi')
     print(response.read())
-  except urllib.error.URLError as error:
+  except URLError as error:
     print("Something went wrong. Try again")
