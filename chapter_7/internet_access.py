aee19049d1f77feb17b25ecafa1346bfecd28659
sai-y <yamanoorsai@gmail.com>
2016-09-20 16:40:15 -0700

    Internet access example

--- chapter_7/internet_access.py
+++ chapter_7/internet_access.py
@@ -6,7 +6,7 @@ from urllib.error import URLError
 if __name__ == "__main__":
   # Source for link: http://stackoverflow.com/a/30635751/822170
   try:
-    response = request.urlopen('http://nist.time.gv/actualtime.cgi')
+    response = request.urlopen('http://nist.time.gov/actualtime.cgi')
     print(response.read())
   except URLError as error:
     print("Something went wrong. Try again")
