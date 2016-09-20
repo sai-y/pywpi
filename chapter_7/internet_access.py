01ad46b4f39392a797ef454f3eeaf73b1d86e705
sai-y <yamanoorsai@gmail.com>
2016-09-20 15:48:16 -0700

    Internet access example

--- chapter_7/internet_access.py
+++ chapter_7/internet_access.py
@@ -2,7 +2,6 @@
 
 from urllib import request
 
-
-# access google.com
+# Source for link: http://stackoverflow.com/a/30635751/822170
 with request.urlopen('http://nist.time.gov/actualtime.cgi') as response:
   print(response.read())
