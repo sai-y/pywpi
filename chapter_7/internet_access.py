09b59e44a9b67e39c488e831f72c1976b9a99b7a
sai-y <yamanoorsai@gmail.com>
2016-09-20 16:29:08 -0700

    Internet access example

--- chapter_7/internet_access.py
+++ chapter_7/internet_access.py
@@ -3,5 +3,8 @@
 from urllib import request
 
 # Source for link: http://stackoverflow.com/a/30635751/822170
-with request.urlopen('http://nist.time.gv/actualtime.cgi') as response:
+try:
+  response = request.urlopen('http://nist.time.gv/actualtime.cgi')
   print(response.read())
+except urllib.error.URLError as error:
+  print("Something went wrong. Try again")
