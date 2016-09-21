4eeb24677ebbf9b3f81899501777ee62bf784a4a
sai-y <yamanoorsai@gmail.com>
2016-09-21 11:29:05 -0700

    Slack message

--- chapter_7/slack_test.py
+++ chapter_7/slack_test.py
@@ -6,7 +6,7 @@ import json
 URL = 'https://hooks.slack.com/services/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'
 
 if __name__ == "__main__":
-  payload = {"text": "The cat door just opened!"}
+  payload = {"text": "The cat door was just opened!"}
   try:
     response = requests.post(URL, json.dumps(payload))
     print(response.text)
