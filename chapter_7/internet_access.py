14b8a4f285464fc9093ec9ad6d44c933ea688d6b
sai-y <yamanoorsai@gmail.com>
2016-09-20 17:12:45 -0700

    Adding Requests

--- chapter_7/internet_access.py
+++ chapter_7/internet_access.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-from urllib import request
-from urllib.error import URLError
+import requests
 
 if __name__ == "__main__":
   # Source for link: http://stackoverflow.com/a/30635751/822170
   try:
-    response = request.urlopen('http://nist.time.gov/actualtime.cgi')
+    response = requests.get('http://nist.time.gv/actualtime.cgi')
     print(response.read())
-  except URLError as error:
+  except requests.HTTPError as error:
     print("Something went wrong. Try again")
