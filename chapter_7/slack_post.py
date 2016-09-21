5489832208026b23911b9f0227072999b9bdf573
sai-y <yamanoorsai@gmail.com>
2016-09-21 16:11:48 -0700

    Slack Post

--- chapter_7/slack_post.py
+++ chapter_7/slack_post.py
@@ -3,11 +3,13 @@
 import requests
 import json
 
+# generate your own url
 URL = 'https://hooks.slack.com/services/'
 
 if __name__ == "__main__":
   payload = {"text": "The cat door was just opened!"}
   try:
+    # encode data payload and post it
     response = requests.post(URL, json.dumps(payload))
     print(response.text)
   except requests.exceptions.ConnectionError as error:
