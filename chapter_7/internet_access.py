878704e933760b97e512dcb760ca5c4df12094ac
sai-y <yamanoorsai@gmail.com>
2016-09-20 18:30:47 -0700

    Adding Request Exception

--- chapter_7/internet_access.py
+++ chapter_7/internet_access.py
@@ -5,7 +5,7 @@ import requests
 if __name__ == "__main__":
   # Source for link: http://stackoverflow.com/a/30635751/822170
   try:
-    response = requests.get('http://nist.time.gv/actualtime.cgi')
+    response = requests.get('http://nist.time.gov/actualtime.cgi')
     print(response.read())
   except requests.exceptions.ConnectionError as error:
     print("Something went wrong. Try again")
