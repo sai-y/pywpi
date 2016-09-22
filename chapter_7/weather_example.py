9aef48c269827b17314852b7c11fc2f15eb40df0
sai-y <yamanoorsai@gmail.com>
2016-09-21 17:22:13 -0700

    Weather Example

--- chapter_7/weather_example.py
+++ chapter_7/weather_example.py
@@ -7,7 +7,7 @@ import json
 APP_ID = '5d6f02fd4472611a20f4ce602010ee0c'
 ZIP = 94103
 URL = """http://api.openweathermap.org/data/2.5/weather?zip={}&appid={}\
-      &units=imperial""".format(ZIP, APP_ID)
+&units=imperial""".format(ZIP, APP_ID)
 
 if __name__ == "__main__":
   # API Documentation: http://openweathermap.org/current#current_JSON
