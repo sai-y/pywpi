bff9834627cbad91be4abe4900eb1df187508167
sai-y <yamanoorsai@gmail.com>
2016-09-21 17:12:45 -0700

    Weather Example

---
+++ chapter_7/weather_example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import requests
+import json 
+
+# generate your own url
+APP_ID = '5d6f02fd4472611a20f4ce602010ee0c'
+URL = "http://api.openweathermap.org/data/2.5/weather?\zip=94103&appid={1}&units=imperial".format(1=APP_ID)
+if __name__ == "__main__":
+  # API Documentation: http://openweathermap.org/current#current_JSON
+  try:
+    # encode data payload and post it
+    response = requests.get(URL)
+    print(response.text)
+  except requests.exceptions.ConnectionError as error:
+    print("The error is %s" % error)
