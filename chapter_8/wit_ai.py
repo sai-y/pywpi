fa3baf49c9094cdedda52457a04b18f05e802aeb
sai-y <yamanoorsai@gmail.com>
2016-10-02 21:38:06 -0700

    Change of version

--- chapter_8/wit_ai.py
+++ chapter_8/wit_ai.py
@@ -3,7 +3,7 @@
 import requests
 
 if __name__ == "__main__":
-  url = 'https://api.wit.ai/speech?v=20160526'
+  url = 'https://api.wit.ai/speech?v=20161002'
   headers = {"Authorization": "Bearer PYQ2OPAUWBB2MSUWVANLMC3FZ2ZB7U5O",
              "Content-Type": "audio/wav"}
   files = {'file': open('sp01.wav', 'rb')}
