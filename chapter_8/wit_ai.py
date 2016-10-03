67e16d1d1e89ba7719002a2b50810172d940d7b5
sai-y <yamanoorsai@gmail.com>
2016-10-02 21:46:58 -0700

    Change of version

--- chapter_8/wit_ai.py
+++ chapter_8/wit_ai.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
   url = 'https://api.wit.ai/speech?v=20161002'
   headers = {"Authorization": "Bearer PYQ2OPAUWBB2MSUWVANLMC3FZ2ZB7U5O",
              "Content-Type": "audio/wav"}
-  files = {'file': open('sp01.wav', 'rb')}
-  response = requests.post(url, headers=headers, files=files)
+  files = {'file': open('sp02.wav', 'rb')}
+  response = requests.post(url, headers=headers, data=files)
   print(response.status_code)
   print(response.text)
