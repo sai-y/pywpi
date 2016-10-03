ff6258d0532613921a50a0ed61f0af89e526e929
sai-y <yamanoorsai@gmail.com>
2016-10-02 21:23:06 -0700

    Chapter 8 example

--- chapter_8/wit_ai.py
+++ chapter_8/wit_ai.py
@@ -3,3 +3,9 @@
 import requests
 
 if __name__ == "__main__":
+  url = 'https://api.wit.ai/speech?v=20160526'
+  headers = {"Authorization": "Bearer PYQ2OPAUWBB2MSUWVANLMC3FZ2ZB7U5O",
+             "Content-Type": "audio/wav"}
+  files = {'file': open('sp01.wav', 'rb')}
+  response = requests.post(url, headers=headers, files=files)
+  print(response.text)
