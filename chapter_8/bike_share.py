6ab703b08745ca11d9b454ff6d6b2a69e2ff51fc
sai-y <yamanoorsai@gmail.com>
2016-10-13 01:12:05 -0700

    Bike share example

--- chapter_8/bike_share.py
+++ chapter_8/bike_share.py
@@ -5,5 +5,7 @@ import requests
 URL = "http://feeds.bayareabikeshare.com/stations/stations.json"
 
 if __name__ == "__main__":
+  # fetch the bike share information
   response = requests.get(URL)
-  print(response.json())
+  parsed_data = response.json()
+  print(parsed_data[1])
