6c759e2aa37693d7d85f25b2cfeb810ddc9462a1
sai-y <yamanoorsai@gmail.com>
2016-10-13 19:21:50 -0700

    Bike Share example

--- chapter_8/bike_share.py
+++ chapter_8/bike_share.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 
 import requests
+import json
 
-URL = "http://feeds.bayareabikeshare.com/stations/stations.json"
+BIKE_URL = "http://feeds.bayareabikeshare.com/stations/stations.json"
+# find your key from ifttt
+IFTTT_URL = """https://maker.ifttt.com/trigger/mobile_notify\
+/with/key/c6ubRi6AhjzkI73Ap1CsaG"""
 
 if __name__ == "__main__":
   # fetch the bike share information
-  response = requests.get(URL)
+  response = requests.get(BIKE_URL)
   parsed_data = response.json()
-  print(parsed_data['stationBeanList'][2])
+  station_list = parsed_data['stationBeanList']
+  for station in station_list:
+    if station['id'] == 65 and\
+       station['availableBikes'] < 10:
+       payload = {"value1": "2"}
+       response = requests.post(IFTTT_URL, json=payload)
+       if response.status_code == 200:
+        print("Notification successfully triggered")
