95c434c9c4381e39fc155ff42e9eac933e4360e2
sai-y <yamanoorsai@gmail.com>
2016-10-16 21:31:41 -0700

    Word of the day

--- chapter_8/wordOfTheDay.py
+++ chapter_8/wordOfTheDay.py
@@ -8,4 +8,4 @@ apiUrl = 'http://api.wordnik.com/v4'
 if __name__ == "__main__":
   client = swagger.ApiClient(API_KEY, apiUrl)
   wordsApi = WordsApi.WordsApi(client)
-  example = wordsApi.wordOfTheDay('2016-10-16')
+  example = wordsApi.getWordOfTheDay()
