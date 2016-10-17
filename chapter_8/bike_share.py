b58d08a24808237823c7919f41d24dd2c528bdec
sai-y <yamanoorsai@gmail.com>
2016-10-16 20:39:37 -0700

    Bike Share example

--- chapter_8/bike_share.py
+++ chapter_8/bike_share.py
@@ -5,8 +5,7 @@ import datetime
 
 BIKE_URL = "http://feeds.bayareabikeshare.com/stations/stations.json"
 # find your key from ifttt
-IFTTT_URL = """https://maker.ifttt.com/trigger/mobile_notify\
-/with/key/c6ubRi6AhjzkI73Ap1CsaG"""
+IFTTT_URL = "https://maker.ifttt.com/trigger/mobile_notify/with/key/$KEY"
 
 if __name__ == "__main__":
   # fetch the bike share information
@@ -15,7 +14,7 @@ if __name__ == "__main__":
   station_list = parsed_data['stationBeanList']
   for station in station_list:
     if station['id'] == 65 and\
-       station['availableBikes'] < 10:
+       station['availableBikes'] < 3:
       print("The available bikes is %d" % station['availableBikes'])
       payload = {"value1": station['availableBikes']}
       response = requests.post(IFTTT_URL, json=payload)
