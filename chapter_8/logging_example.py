00b47e3fa2502a2f48ca30caa2d3bd439218c190
sai-y <yamanoorsai@gmail.com>
2016-10-25 20:21:02 -0700

    Logging example

--- chapter_8/logging_example.py
+++ chapter_8/logging_example.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import requests
 import logging
@@ -9,8 +9,8 @@ IFTTT_URL = "https://maker.ifttt.com/trigger/rf_trigger/with/key/cmFd9FSYZzzn_Kh
 if __name__ == "__main__":
   # fetch the bike share information
   logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', 
-                      filename=BEATS_LOG_FILE,
-                      level=logging.WARNING)
+                      filename='log_file.log',
+                      level=logging.INFO)
   payload = {"value1": "Sample_1", "value2": "Sample_2"}
   response = requests.post(IFTTT_URL, json=payload)
   if response.status_code == 200:
