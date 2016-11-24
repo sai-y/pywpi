e8b400fdfc78f389a1185959aace7627977b2cdd
sai-y <yamanoorsai@gmail.com>
2016-11-23 19:11:13 -0800

    OpenCV Test

--- chapter_8/opencv_test.py
+++ chapter_8/opencv_test.py
@@ -2,7 +2,7 @@
 
 import cv2
 
-img = cv2.imread('/home/pi/Desktop/test_shot.jpg',cv2.IMREAD_GRAYSCALE)
-cv2.imshow('image',img)
+img = cv2.imread('/home/pi/Desktop/test_shot.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
