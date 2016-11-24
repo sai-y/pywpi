bd8113d975d84437ee73c54fdbcdc079d4b31fec
sai-y <yamanoorsai@gmail.com>
2016-11-23 20:50:43 -0800

    Threading example

--- chapter_8/threading_example.py
+++ chapter_8/threading_example.py
@@ -4,7 +4,7 @@ import threading
 
 def thread_function(string):
   for num in range(5):
-    print("%s: Iteration: %d", %(string, num))
+    print("%s: Iteration: %d" %(string, num))
 
 if __name__ == '__main__':
   thread_1 = threading.Thread(target=worker, args=("Thread 1",))
