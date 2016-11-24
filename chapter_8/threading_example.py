759d29d19ab9a4a1c8d787b673b5411023682886
sai-y <yamanoorsai@gmail.com>
2016-11-23 20:51:36 -0800

    Threading example

--- chapter_8/threading_example.py
+++ chapter_8/threading_example.py
@@ -7,11 +7,11 @@ def thread_function(string):
     print("%s: Iteration: %d" %(string, num))
 
 if __name__ == '__main__':
-  thread_1 = threading.Thread(target=worker, args=("Thread 1",))
+  thread_1 = threading.Thread(target=thread_function, args=("Thread 1",))
   thread_1.start()
 
-  thread_2 = threading.Thread(target=worker, args=("Thread 2",))
+  thread_2 = threading.Thread(target=thread_function, args=("Thread 2",))
   thread_2.start()
 
-  thread_3 = threading.Thread(target=worker, args=("Thread 3",))
+  thread_3 = threading.Thread(target=thread_function, args=("Thread 3",))
   thread_3.start()
