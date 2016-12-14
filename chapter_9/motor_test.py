060e5a053b27bcf8bafe0fc545673e03944ab9c5
sai-y <yamanoorsai@gmail.com>
2016-12-14 09:22:53 -0800

    Motor Test

---
+++ chapter_9/motor_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from AdaFruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+from time import sleep
+
+
+if __name__ == "__main__":
+  motor_driver = AdaFruit_MotorHAT(addr=0x60)
+
+  left_motor = motor_driver.getMotor(1)
+  right_motor = motor_driver.getMotor(2)
+
+  left_motor.setSpeed(255)
+  right_motor.setSpeed(255)
+
+  left_motor.run(Adafruit_MotorHAT.FORWARD)
+  right_motor.run(Adafruit_MotorHAT.FORWARD)
+
+  left_motor.run(Adafruit_MotorHAT.RELEASE)
+  right_motor.run(Adafruit_MotorHAT.RELEASE)
+
+  sleep(10)
+
+  left_motor.setSpeed(200)
+  right_motor.setSpeed(200)
+
+  left_motor.run(Adafruit_MotorHAT.FORWARD)
+  right_motor.run(Adafruit_MotorHAT.FORWARD)
+
+  sleep(10)
+
+  left_motor.run(Adafruit_MotorHAT.RELEASE)
+  right_motor.run(Adafruit_MotorHAT.RELEASE)
+
+
+
+
+
+
+
+
+
