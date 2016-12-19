82fe460023f73790f3ae7e091030214ee270edb4
sai-y <yamanoorsai@gmail.com>
2016-12-18 23:03:07 -0800

    Web interface final

--- chapter_9/web_interface.py
+++ chapter_9/web_interface.py
@@ -7,9 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    author = "Me"
-    name = "You"
-    return render_template('index.html', author=author, name=name)
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
