f4871f8c16c1ddffcbc4804fda4989d3d89ed462
sai-y <yamanoorsai@gmail.com>
2016-12-18 19:12:16 -0800

    Web Interface first commit

---
+++ chapter_9/web_interface.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+	Flask framework
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run('0.0.0.0')
