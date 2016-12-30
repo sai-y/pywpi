7d45d8d5cab9ca46abccd13b06699ad634b1a402
sai-y <sai@altschool.com>
2016-12-30 12:48:29 -0800

    Removal of robot class

--- chapter_10/flask_framework_appliance/web_interface.py
+++ chapter_10/flask_framework_appliance/web_interface.py
@@ -5,7 +5,7 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-my_robot = Robot(1,2)
+
 
 @app.route("/")
 def hello():
@@ -13,22 +13,18 @@ def hello():
 
 @app.route('/forward', methods = ['POST'])
 def forward():
-    my_robot.forward(0.25)
     return redirect('/')
 
 @app.route('/reverse', methods = ['POST'])
 def reverse():
-    my_robot.reverse(0.25)
     return redirect('/')
 
 @app.route('/left', methods = ['POST'])
 def left():
-    my_robot.left(0.25)
     return redirect('/')
 
 @app.route('/right', methods = ['POST'])
 def right():
-    my_robot.right(0.25)
     return redirect('/')
 
 if __name__ == "__main__":
