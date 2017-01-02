3ac7dd83d40e932aabf2a995f41d1f5c22ad843a
sai-y <sai@altschool.com>
2017-01-02 10:48:49 -0800

    Adding timer to visual aid

--- chapter_10/personal_health/fitbit_client.py
+++ chapter_10/personal_health/fitbit_client.py
@@ -7,10 +7,10 @@ import fitbit
 import datetime
 
 # insert your keys here
-CONSUMER_KEY = "***REMOVED***"
-CONSUMER_SECRET = "***REMOVED***"
-REFRESH_TOKEN = "***REMOVED***"
-ACCESS_TOKEN = "***REMOVED***"
+CONSUMER_KEY = "229VMS"
+CONSUMER_SECRET = "45ae7950f85f825638ff0e8087efad4c"
+REFRESH_TOKEN = "bbb3dea74025ca96270f4f594c9a3deccc06f9f2afcbc1ca2e24ad8f9bd8ffa9"
+ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzTk1RQ1QiLCJhdWQiOiIyMjlWTVMiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNDgzMzYyNjIzLCJpYXQiOjE0ODMzMzM4MjN9.cdUihcllL6D_c9Pgm_zLlYabiwacxXk1Shap-hVzNLA"
 
 
 if __name__ == "__main__":
