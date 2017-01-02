610d0b95eecb51a997cb37c480f31b37440f4c12
sai-y <sai@altschool.com>
2017-01-01 22:58:15 -0800

    Personal health tracker - JSON parsing

--- chapter_10/personal_health/fitbit_client.py
+++ chapter_10/personal_health/fitbit_client.py
@@ -8,10 +8,10 @@ import json
 import datetime
 
 # insert your keys here
-CONSUMER_KEY = "***REMOVED***"
-CONSUMER_SECRET = "***REMOVED***"
-REFRESH_TOKEN = "***REMOVED***"
-ACCESS_TOKEN = "***REMOVED***"
+CONSUMER_KEY = "229VMS"
+CONSUMER_SECRET = "45ae7950f85f825638ff0e8087efad4c"
+REFRESH_TOKEN = "bbb3dea74025ca96270f4f594c9a3deccc06f9f2afcbc1ca2e24ad8f9bd8ffa9"
+ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzTk1RQ1QiLCJhdWQiOiIyMjlWTVMiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNDgzMzYyNjIzLCJpYXQiOjE0ODMzMzM4MjN9.cdUihcllL6D_c9Pgm_zLlYabiwacxXk1Shap-hVzNLA"
 
 
 if __name__ == "__main__":
@@ -25,4 +25,4 @@ if __name__ == "__main__":
 											 detail_level='15min',
 											 start_time="00:00",
 											 end_time=end_time)
-	print(response['activities-steps']['value'])
+	print(response['activities-steps'][0]['value'])
