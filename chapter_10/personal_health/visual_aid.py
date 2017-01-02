8b4893e2df955746e709498c3d0085db7ce5935f
sai-y <sai@altschool.com>
2017-01-02 10:45:20 -0800

    Adding timer to visual aid

--- chapter_10/personal_health/visual_aid.py
+++ chapter_10/personal_health/visual_aid.py
@@ -15,23 +15,45 @@ REFRESH_TOKEN = "bbb3dea74025ca96270f4f594c9a3deccc06f9f2afcbc1ca2e24ad8f9bd8ffa
 ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzTk1RQ1QiLCJhdWQiOiIyMjlWTVMiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNDgzMzYyNjIzLCJpYXQiOjE0ODMzMzM4MjN9.cdUihcllL6D_c9Pgm_zLlYabiwacxXk1Shap-hVzNLA"
 
 if __name__ == "__main__":
-    fbit_client = fitbit.Fitbit(CONSUMER_KEY,
-                                CONSUMER_SECRET,
-                                access_token=ACCESS_TOKEN,
-                                refresh_token=REFRESH_TOKEN)
+    client = fitbit.Fitbit(CONSUMER_KEY,
+                           CONSUMER_SECRET,
+                           access_token=ACCESS_TOKEN,
+                           refresh_token=REFRESH_TOKEN)
     blinkt.set_brightness(0.1)
-    for i in range(7):
-        blinkt.set_pixel(i, 0, 255, 0)
+    current_time = time.time()
+
+    num_leds = 0
+
     while True:
         now = datetime.datetime.now()
         end_time = now.strftime("%H:%M")
-        response = fbit_client.intraday_time_series('activities/steps',
-                                                    detail_level='15min',
-                                                    start_time="00:00",
-                                                    end_time=end_time)
-        blinkt.set_pixel(7, 255, 0, 0)
-        blinkt.show()
-        time.sleep(1)
-        blinkt.set_pixel(7, 0, 0, 0)
-        blinkt.show()
-        time.sleep(1)
+
+        # update steps every 15 minutes
+        if (time.time() - current_time) > 900:
+
+        try:
+            response = client.intraday_time_series('activities/steps',
+                                                   detail_level='15min',
+                                                   start_time="00:00",
+                                                   end_time=end_time)
+        except Exception as error:
+            print(error)
+        else:
+            current_time  = time.time()
+            steps = response['activities-steps'][0]['value']
+            num_leds = steps // 1250
+            print(steps)
+        
+        if num_leds > 8:
+            num_leds = 8
+        
+        for i in range(num_leds):
+            blinkt.set_pixel(i, 0, 255, 0)
+        
+        if num_leds <= 7: 
+            blinkt.set_pixel(num_leds, 255, 0, 0)
+            blinkt.show()
+            time.sleep(1)
+            blinkt.set_pixel(num_leds, 0, 0, 0)
+            blinkt.show()
+            time.sleep(1)
