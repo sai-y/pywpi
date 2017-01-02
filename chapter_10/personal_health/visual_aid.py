d60a013be4d08f6e317f461e99a50e7a8d74c5d8
sai-y <sai@altschool.com>
2017-01-02 10:48:49 -0800

    Adding timer to visual aid

--- chapter_10/personal_health/visual_aid.py
+++ chapter_10/personal_health/visual_aid.py
@@ -9,10 +9,10 @@ import fitbit
 import time
 
 # insert your keys here
-CONSUMER_KEY = "***REMOVED***"
-CONSUMER_SECRET = "***REMOVED***"
-REFRESH_TOKEN = "***REMOVED***"
-ACCESS_TOKEN = "***REMOVED***"
+CONSUMER_KEY = "229VMS"
+CONSUMER_SECRET = "45ae7950f85f825638ff0e8087efad4c"
+REFRESH_TOKEN = "bbb3dea74025ca96270f4f594c9a3deccc06f9f2afcbc1ca2e24ad8f9bd8ffa9"
+ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzTk1RQ1QiLCJhdWQiOiIyMjlWTVMiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNDgzMzYyNjIzLCJpYXQiOjE0ODMzMzM4MjN9.cdUihcllL6D_c9Pgm_zLlYabiwacxXk1Shap-hVzNLA"
 
 if __name__ == "__main__":
     client = fitbit.Fitbit(CONSUMER_KEY,
@@ -25,24 +25,22 @@ if __name__ == "__main__":
     num_leds = 0
 
     while True:
-        now = datetime.datetime.now()
-        end_time = now.strftime("%H:%M")
-
         # update steps every 15 minutes
         if (time.time() - current_time) > 900:
-
-        try:
-            response = client.intraday_time_series('activities/steps',
-                                                   detail_level='15min',
-                                                   start_time="00:00",
-                                                   end_time=end_time)
-        except Exception as error:
-            print(error)
-        else:
-            current_time  = time.time()
-            steps = response['activities-steps'][0]['value']
-            num_leds = steps // 1250
-            print(steps)
+            try:
+                now = datetime.datetime.now()
+                end_time = now.strftime("%H:%M")
+                response = client.intraday_time_series('activities/steps',
+                                                       detail_level='15min',
+                                                       start_time="00:00",
+                                                       end_time=end_time)
+            except Exception as error:
+                print(error)
+            else:
+                current_time  = time.time()
+                steps = response['activities-steps'][0]['value']
+                num_leds = steps // 1250
+                print(steps)
         
         if num_leds > 8:
             num_leds = 8
