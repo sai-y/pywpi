0e48bf6b1a1ae68e87ccf890915dcab7d87f99c5
sai-y <sai@altschool.com>
2017-01-01 22:54:44 -0800

    Personal health tracker - JSOn parsing

--- chapter_10/personal_health/fitbit_client.py
+++ chapter_10/personal_health/fitbit_client.py
@@ -15,14 +15,14 @@ ACCESS_TOKEN = "***REMOVED***"
 
 
 if __name__ == "__main__":
-	fitbit_client = fitbit.Fitbit(CONSUMER_KEY,
+	fbit_client = fitbit.Fitbit(CONSUMER_KEY,
 								  CONSUMER_SECRET,
                              	  access_token=ACCESS_TOKEN,
                              	  refresh_token=REFRESH_TOKEN)
 	now = datetime.datetime.now()
 	end_time = now.strftime("%H:%M")
-	print(end_time)
-	print(fitbit_client.intraday_time_series('activities/steps',
+	response = json.load(fbit_client.intraday_time_series('activities/steps',
 											 detail_level='15min',
 											 start_time="00:00",
 											 end_time=end_time))
+	print(response)
