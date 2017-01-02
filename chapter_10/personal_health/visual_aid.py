35faf853d4dacbd7bc9108b392761d968b09ce71
sai-y <sai@altschool.com>
2017-01-02 09:29:23 -0800

    Visual aid tool

--- chapter_10/personal_health/visual_aid.py
+++ chapter_10/personal_health/visual_aid.py
@@ -4,10 +4,16 @@
 """
 
 import blinkt
-
+import time
 
 if __name__ == "__main__":
     blinkt.set_brightness(0.1)
-    for i in range(7):
+    for i in range(6):
         blinkt.set_pixel(i, 0, 255, 0)
+    while True:
+        blinkt.set_pixel(7, 255, 0, 0)
         blinkt.show()
+        time.sleep(1)
+        blinkt.set_pixel(7, 0, 0, 0)
+        blinkt.show()
+        time.sleep(1)
