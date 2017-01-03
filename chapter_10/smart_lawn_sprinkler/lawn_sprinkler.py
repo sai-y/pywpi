eb5c988feb4cab62e7948be68091e98ee7962245
sai-y <sai@altschool.com>
2017-01-02 19:46:52 -0800

    Chapter 10 commit

--- chapter_10/smart_lawn_sprinkler/lawn_sprinkler.py
+++ chapter_10/smart_lawn_sprinkler/lawn_sprinkler.py
@@ -7,7 +7,7 @@ import requests
 import schedule
 import time
 
-URL = ("https://api.darksky.net/forecast/***REMOVED***"
+URL = ("https://api.darksky.net/forecast/key"
 "/37.8267,-122.4233?exclude=currently,minutely,hourly")
 
 def check_weather():
