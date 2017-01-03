3b4c2ab70be1ba667ed19d8e5caf51c6fc3aa488
sai-y <sai@altschool.com>
2017-01-02 18:01:32 -0800

    Lawn Sprinkler

---
+++ chapter_10/smart_lawn_sprinkler/lawn_sprinkler.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Smart Water Sprinkler Example
+"""
+
+import requests
+import schedule
+
+
+URL = ("https://api.darksky.net/forecast/08b2b3024c2a1b85dabf14cea653c899"
+"/37.8267,-122.4233?exclude=currently,minutely,hourly")
+
+if __name__ == "__main__":
+	response = requests.get(URL)
+	data = response.json()
+	print(data["daily"]["data"][1]["rain"])
