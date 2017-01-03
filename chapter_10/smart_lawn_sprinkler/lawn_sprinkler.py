a7753973abad1b37509868cca4ce7746a5882b54
sai-y <sai@altschool.com>
2017-01-02 18:17:52 -0800

    Lawn sprinkler

--- chapter_10/smart_lawn_sprinkler/lawn_sprinkler.py
+++ chapter_10/smart_lawn_sprinkler/lawn_sprinkler.py
@@ -5,12 +5,33 @@
 
 import requests
 import schedule
-
+import time
 
 URL = ("https://api.darksky.net/forecast/08b2b3024c2a1b85dabf14cea653c899"
 "/37.8267,-122.4233?exclude=currently,minutely,hourly")
 
-if __name__ == "__main__":
+def check_weather():
 	response = requests.get(URL)
 	data = response.json()
-	print(data["daily"]["data"][1]["icon"])
+	if data["daily"]["data"][1]["icon"] == "rain":
+		return True
+	else:
+		return False
+
+def turn_on_sprinkler():
+	if not check_weather():
+		# turn on sprinkler
+		time.sleep(600)
+		# turn off sprinkler
+	else:
+		print("Ignoring the sprinker for today")
+
+def turn_off_sprinkler():
+	pass
+
+if __name__ == "__main__":
+	schedule.every().day.at("18:21").do(turn_on_sprinkler)
+
+	while True:
+		schedule.run_pending()
+		time.sleep(1)
