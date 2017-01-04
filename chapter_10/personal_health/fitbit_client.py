d9afc517dd2fe7e93eeb8cdf286c08fb6216e91c
sai-y <sai@altschool.com>
2017-01-03 20:57:43 -0800

    Visual aid tracker

--- chapter_10/personal_health/fitbit_client.py
+++ chapter_10/personal_health/fitbit_client.py
@@ -7,10 +7,10 @@ import fitbit
 import datetime
 
 # insert your keys here
-CONSUMER_KEY = "KEY"
-CONSUMER_SECRET = "KEY"
-REFRESH_TOKEN = "KEY"
-ACCESS_TOKEN = "KEY"
+CONSUMER_KEY = "***REMOVED***"
+CONSUMER_SECRET = "***REMOVED***"
+ACCESS_TOKEN = "***REMOVED***"
+REFRESH_TOKEN = "***REMOVED***"
 
 
 if __name__ == "__main__":
