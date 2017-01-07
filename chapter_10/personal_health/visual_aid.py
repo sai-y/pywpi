c11bd8da74d7c28277ebdb4700965b6e1f91c73d
sai-y <sai@altschool.com>
2017-01-07 02:36:28 -0800

    Adding config parser to visual_aid tracker

--- chapter_10/personal_health/visual_aid.py
+++ chapter_10/personal_health/visual_aid.py
@@ -7,15 +7,17 @@ import blinkt
 import datetime
 import fitbit
 import time
-import schedule
+import ConfigParser
 
-# insert your keys here
+# config is loaded from config file
+# alternatively you may store them as constants in your program
+config = ConfigParser.ConfigParser()
+config.read('config.ini')
 
-CONSUMER_KEY = "229VMS"
-CONSUMER_SECRET = "45ae7950f85f825638ff0e8087efad4c"
-ACCESS_TOKEN= "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzTk1RQ1QiLCJhdWQiOiIyMjlWTVMiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJwcm8gcm51dCByc2xlIiwiZXhwIjoxNDgzNTQ4MjQ1LCJpYXQiOjE0ODM1MTk0NDV9.kKI1PG0UNOloJ8LAhyq_oHD0NXPcez-_gzb_2oKRgHM"
-REFRESH_TOKEN = "98eafe6d9e68f617021d9f6316869a6cec2ff05c637eebd40ebebff05e77ea82"
-
+CONSUMER_KEY = config.get("APP", "CONSUMER_KEY")
+CONSUMER_SECRET = config.get("APP", "CONSUMER_SECRET")
+REFRESH_TOKEN = config.get("USER", "REFRESH_TOKEN")
+ACCESS_TOKEN = config.get("USER", "ACCESS_TOKEN")
 
 
 def refresh_token():
@@ -29,14 +31,14 @@ def refresh_token():
 
 def get_steps():
     num_steps = 0
-    
+
     try:
         now = datetime.datetime.now()
         end_time = now.strftime("%H:%M")
         response = client.intraday_time_series('activities/steps',
-                                                detail_level='15min',
-                                                start_time="00:00",
-                                                end_time=end_time)
+                                               detail_level='15min',
+                                               start_time="00:00",
+                                               end_time=end_time)
     except Exception as error:
         print(error)
     else:
@@ -48,14 +50,13 @@ def get_steps():
             pass
     return num_steps
 
-
 if __name__ == "__main__":
 
     client = fitbit.Fitbit(CONSUMER_KEY,
                            CONSUMER_SECRET,
                            access_token=ACCESS_TOKEN,
                            refresh_token=REFRESH_TOKEN)
-    
+
     blinkt.set_brightness(0.1)
     current_time = time.time()
 
@@ -66,18 +67,18 @@ if __name__ == "__main__":
         schedule.run_pending()
         # update steps every 15 minutes
         if (time.time() - current_time) > 900:
-            current_time  = time.time()
+            current_time = time.time()
             steps = get_steps()
 
         num_leds = steps // 1250
-      
+
         if num_leds > 8:
             num_leds = 8
-        
+
         for i in range(num_leds):
             blinkt.set_pixel(i, 0, 255, 0)
-        
-        if num_leds <= 7: 
+
+        if num_leds <= 7:
             blinkt.set_pixel(num_leds, 255, 0, 0)
             blinkt.show()
             time.sleep(1)
