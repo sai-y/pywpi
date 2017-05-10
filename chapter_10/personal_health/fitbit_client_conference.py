e6b2ab136a8e6a4b4eeca79230062119af1190b2
sai-y <sai@altschool.com>
2017-05-09 22:07:33 -0700

    Conference project

---
+++ chapter_10/personal_health/fitbit_client_conference.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    Personal health improvement example
+"""
+
+import fitbit
+import datetime
+import configparser
+
+# config is loaded from config file
+# alternatively you may store them as constants in your program
+CONFIG_FILE = '/home/pi/config.ini'
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE)
+
+CONSUMER_KEY = config.get("APP", "CONSUMER_KEY")
+CONSUMER_SECRET = config.get("APP", "CONSUMER_SECRET")
+REFRESH_TOKEN = config.get("USER", "REFRESH_TOKEN")
+ACCESS_TOKEN = config.get("USER", "ACCESS_TOKEN")
+
+
+if __name__ == "__main__":
+    fbit_client = fitbit.Fitbit(CONSUMER_KEY,
+                                CONSUMER_SECRET,
+                                access_token=ACCESS_TOKEN,
+                                refresh_token=REFRESH_TOKEN)
+    # now = datetime.datetime.now()
+    # end_time = now.strftime("%H:%M")
+    response = fbit_client.time_series(
+        'activities/steps',
+        user_id='5HX8YP'
+    )
+    
